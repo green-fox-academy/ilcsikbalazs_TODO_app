249442f52c116dc8d89be04cf4b9c183f6ff10a5
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-04-06 17:34:14 +0200

    remove task works fine

--- TODO_app/backend.py
+++ TODO_app/backend.py
@@ -5,18 +5,14 @@ class Backend:
         output = []
         for i in input_read:
             output.append(i.split(","))
+        tasks_read.close()
         return output      
 
     def write_into_file(self,after_a):
         task_write = open('basic_tasks.txt', 'a')
         append = "0,"+after_a+"\n"
         input_write = task_write.writelines(append)
-
-    def save_file(self):
-        open_file = self.open_separate()
-        for i in open_file:
-            for j in i:
-                print(j)
+        task_write.close()
 
     def remove_a_task(self,remove_this):
         open_file = self.open_separate()
@@ -24,8 +20,17 @@ class Backend:
         for i in open_file:
             if i[1] == string:
                 open_file.remove(i)
-                print(open_file)
 
-
-a = Backend()
-a.save_file()
+        for i in open_file:
+            task_write = open('temporary.txt', 'a')
+            add = i[0]+ "," +i[1]
+            input_write = task_write.writelines(add)
+        
+        task_write.close()
+        task_read = open('temporary.txt', 'r')
+        basic_file = open('basic_tasks.txt', 'w')
+        basic_file.write(task_read.read())
+        basic_file.close()
+        temporary_delete = open('temporary.txt', 'w')
+        temporary_delete.write("")
+        temporary_delete.close()
